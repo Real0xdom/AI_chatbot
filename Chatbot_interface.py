b077ad289da4b67602b0e3d7e664ec1d27adb1ec
Real0xdom <pranavpanchal2000@gmail.com>
2026-04-30 21:30:00 +0530

    first commit

---
+++ Chatbot_interface.py
@@ -0,0 +1,19 @@
+from helper_function import get_llm_response, print_llm_response
+
+chat_history = []
+
+print("welcome to my AI Chatbot!")
+
+while True:
+    
+    user_message = input("you: ")
+    
+    if user_message.lower() in ["bye", "exit", "quit"]:
+        print("see you later, bye!")
+        break
+    chatbot_response = get_llm_response(user_message, chat_history)
+    print(f"Chatbot: {chatbot_response}")
+    print("")
+    chat_history.append({"role": "user", "content": user_message})
+    chat_history.append({"role": "assistant", "content": chatbot_response})
+
---
+++ helper_function.py
@@ -0,0 +1,48 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+#load the env variables
+load_dotenv()
+
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+def get_llm_response(prompt, chat_history):
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-20b:groq",
+        messages=[
+            {
+                "role": "system",
+                "content": "You're an helpful AI assistant only give brief and small answers include emoji"
+            }
+        ] + chat_history[-6:] + [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.2
+    )
+    reply = completion.choices[0].message.content
+    return reply
+
+def print_llm_response(prompt):
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-20b:groq",
+        messages=[
+            {
+                "role": "system",
+                "content": "You're an helpful AI assistant"
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.2
+    )
+    print(completion.choices[0].message.content)
+
