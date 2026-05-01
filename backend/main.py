eeda5381ca3ffea03c4c9c2dd0f86318047b3748
Real0xdom <pranavpanchal2000@gmail.com>
2026-05-01 15:02:10 +0530

    CORS ADD

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,8 @@ chat_history = []
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://ai-chatbot-plum-five-76.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
